7a7ac1821da633f77b3c5a32fa8ef7d5e2c5face
Luana Venâncio <luagabriela060@gmail.com>
2023-11-02 19:57:18 -0300

    Seller and department DAO

---
+++ src/model/dao/DepatmentDao.java
@@ -0,0 +1,14 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entities.Department;
+
+public interface DepatmentDao {
+	
+	void insert(Department obj);
+	void update(Department obj);
+	void deletById(Integer id);
+	Department findById(Integer id);
+	List<Department> findAll();
+}
