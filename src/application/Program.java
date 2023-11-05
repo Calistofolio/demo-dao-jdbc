91faf691e2cf88502fa607d425574bb6f2c22a1d
Luana Venâncio <luagabriela060@gmail.com>
2023-11-05 15:59:14 -0300

    Insert into

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -17,7 +18,7 @@ public class Program {
 		
 		System.out.println(seller);
 		
-		System.out.println("Teste 2");
+		System.out.println("\n Teste 2");
 		Department department = new Department(2, null);
 		
 		List<Seller> list = sellerDao.findByDepartment(department);
@@ -26,12 +27,17 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("Teste 3");
+		System.out.println("\n Teste 3");
 		list = sellerDao.findAll();
 			
 			for(Seller obj : list) {
 				System.out.println(obj);
 			}
+			
+			System.out.println("\n teste 4");
+			Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+			sellerDao.insert(newSeller);
+			System.out.println("New id = " + newSeller.getId());
 	}
 
 }
